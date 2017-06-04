43df633424d87b053246937c41ba3db4d1d3836a
gjack <gabi@gabijack.com>
2017-06-03 23:11:12 -0400

    SearchBar component

--- app/index.js
+++ app/index.js
@@ -2,14 +2,15 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 require('./index.css');
 import { youtube_api_key as API_KEY } from './config.js';
+import SearchBar from './components/search_bar.js';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello World!
+        <SearchBar />
       </div>
-    )
+    );
   }
 }
 
