b4e1b87e9fea2a8c0edecda59896228350380acb
gjack <gabi@gabijack.com>
2017-06-05 22:33:36 -0400

    Fetch initial list of videos to work on UI components

--- app/index.js
+++ app/index.js
@@ -1,10 +1,22 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 require('./index.css');
 import { youtube_api_key as API_KEY } from './config.js';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar.js';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    });
+  }
+
   render() {
     return (
       <div>
